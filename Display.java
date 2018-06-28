0d85a23720007f58a2349a86b7ab7de1ee6876d6
Tristan Lonsway <tblonswa@syr.edu>
2018-06-28 04:53:54 -0400

    Disabled automatic visual zoom feature and added image saving after generation is complete

--- Display.java
+++ Display.java
@@ -22,43 +22,7 @@ public class Display extends JComponent {
             double y = d[1];
             int i = (int)d[2];
             //i=i%18;
-            while(i>16&&i!=maxit) {
-                i-=16;
-            }
-            if (i==1)
-                g.setColor(new Color(66,30,15));
-            if (i==2)
-                g.setColor(new Color(25,7,26));
-            if (i==3)
-                g.setColor(new Color(9,1,47));
-            if (i==4)
-                g.setColor(new Color(4,4,73));
-            if (i==5)
-                g.setColor(new Color(0,7,100));
-            if (i==6)
-                g.setColor(new Color(12,44,138));
-            if (i==7)
-                g.setColor(new Color(24,82,177));
-            if (i==8)
-                g.setColor(new Color(57,125,209));
-            if (i==9)
-                g.setColor(new Color(134,181,229));
-            if (i==10)
-                g.setColor(new Color(211,236,248));
-            if (i==11)
-                g.setColor(new Color(241,233,191));
-            if (i==12)
-                g.setColor(new Color(248,201,95));
-            if (i==13)
-                g.setColor(new Color(255,170,0));
-            if (i==14)
-                g.setColor(new Color(204,128,0));
-            if (i==15)
-                g.setColor(new Color(153,87,0));
-            if (i==16)
-                g.setColor(new Color(106,52,3));
-            if (i==maxit)
-                g.setColor(new Color(0,0,0));
+            g.setColor(getIterationColor(i));
             //g.setColor(new Color((int)i,0,0));
             g.fillRect((int)x, (int)y, 1, 1);
         }
@@ -68,5 +32,45 @@ public class Display extends JComponent {
     }
     public void setPoints(ArrayList<double[]> d) {
         points=d;
+    }
+    public Color getIterationColor(int i) {
+        while(i>16&&i!=maxit) {
+            i-=16;
+        }
+        if (i==1)
+            return (new Color(66,30,15));
+        if (i==2)
+            return (new Color(25,7,26));
+        if (i==3)
+            return (new Color(9,1,47));
+        if (i==4)
+            return (new Color(4,4,73));
+        if (i==5)
+            return (new Color(0,7,100));
+        if (i==6)
+            return (new Color(12,44,138));
+        if (i==7)
+            return (new Color(24,82,177));
+        if (i==8)
+            return (new Color(57,125,209));
+        if (i==9)
+            return (new Color(134,181,229));
+        if (i==10)
+            return (new Color(211,236,248));
+        if (i==11)
+            return (new Color(241,233,191));
+        if (i==12)
+            return (new Color(248,201,95));
+        if (i==13)
+            return (new Color(255,170,0));
+        if (i==14)
+            return (new Color(204,128,0));
+        if (i==15)
+            return (new Color(153,87,0));
+        if (i==16)
+            return (new Color(106,52,3));
+        if (i==maxit)
+            return (new Color(0,0,0));  
+        return Color.BLACK;
     }
 }
--- Initialize.java
+++ Initialize.java
@@ -1,6 +1,9 @@
 import java.awt.*;
 import javax.swing.*;
 import java.util.*;
+import java.awt.image.BufferedImage;
+import javax.imageio.*;
+import java.io.*;
 public class Initialize {
     public static void main(String[] args) {
         JFrame frame = new JFrame("window");
@@ -22,33 +25,46 @@ public class Initialize {
         double right=2;
         double top=1;
         double bottom=-1;        
-        for(int i=0;i<100;i++) {
-            double vertInc = (top-bottom)/HEIGHT;
-            double horzInc = (right-left)/WIDTH;
-            double a = top;
-            double b = left;
-            ArrayList<double[]> points = new ArrayList<double[]>();
-            //System.out.println("vertInc: " + vertInc);
-            //System.out.println("horzInc: " + horzInc);
-            for(int r=0;r<HEIGHT;r++) {
-                for(int c=0;c<WIDTH;c++) {
-                    int calc = Calculate.iterations(new double[]{b,a}, ITERATIONS);
-                    //System.out.println(a + " " + b);
-                    points.add(new double[]{c,r,calc});
-                    //d.draw();
-                    b+=horzInc;
-                }
-                b=left;
-                a-=vertInc;
-                //System.out.println((double)r/(double)HEIGHT*100);
+        //for(int i=0;i<100;i++) {
+        double vertInc = (top-bottom)/HEIGHT;
+        double horzInc = (right-left)/WIDTH;
+        double a = top;
+        double b = left;
+        ArrayList<double[]> points = new ArrayList<double[]>();
+        //System.out.println("vertInc: " + vertInc);
+        //System.out.println("horzInc: " + horzInc);
+        for(int r=0;r<HEIGHT;r++) {
+            for(int c=0;c<WIDTH;c++) {
+                int calc = Calculate.iterations(new double[]{b,a}, ITERATIONS);
+                //System.out.println(a + " " + b);
+                points.add(new double[]{c,r,calc});
+                //d.draw();
+                b+=horzInc;
             }
-            d.setPoints(points);
-            d.setVisible(true);
-            d.draw();
-            left+=.02;
-            right-=.02;
-            bottom+=.01;
-            top-=.01;
+            b=left;
+            a-=vertInc;
+            //System.out.println((double)r/(double)HEIGHT*100);
         }
+        d.setPoints(points);
+        d.setVisible(true);
+        d.draw();
+        //left+=.02;
+        //right-=.02;
+        //bottom+=.01;
+        //top-=.01;
+        BufferedImage out = new BufferedImage(WIDTH,HEIGHT, BufferedImage.TYPE_INT_RGB);
+        for(double[] du : points) {
+            try {
+                out.setRGB((int)du[0], (int)du[1], (d.getIterationColor((int)du[2])).getRGB());
+            } catch (Exception e) {
+            }
+        }
+        File output = new File("mandlebrot_render.png");
+        try {
+            ImageIO.write(out,"png",output);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //}
     }
 }
